a4827d2f9c41dbd66d1fb16c4f8cd396ed1616fe
meriadec <meriadec.pillet@gmail.com>
2014-05-17 02:10:38 +0200

    update field

--- app/scripts/controllers/field.js
+++ app/scripts/controllers/field.js
@@ -1,6 +1,18 @@
 'use strict';
 
 angular.module('fieldApp')
-	.controller('FieldCtrl', function ($scope, data) {
-		console.log(data);
+	.controller('FieldCtrl', function ($scope, data, $http, $location) {
+
+		var original = angular.copy(data);
+
+		$scope.field = data;
+
+		$scope.updateField = function () {
+			$http.put('/api/fields/' + $scope.field._id, $scope.field).then(function (res) {
+				$location.path('/');
+			}, function (err) {
+				console.log(err);
+			});
+		};
+
 	});
