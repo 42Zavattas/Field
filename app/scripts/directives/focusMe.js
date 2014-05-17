6ed0bbaf4925d096d52a095450b034953abbcabe
meriadec <meriadec.pillet@gmail.com>
2014-05-17 18:06:56 +0200

    improve focus directive, style

--- app/scripts/directives/focusMe.js
+++ app/scripts/directives/focusMe.js
@@ -6,16 +6,11 @@ angular.module('fieldApp')
 			link: function(scope, element, attrs) {
 				var model = $parse(attrs.focusMe);
 				scope.$watch(model, function(value) {
-					console.log('value=',value);
 					if(value === true) {
 						$timeout(function() {
 							element[0].focus();
 						});
 					}
-				});
-				element.bind('blur', function() {
-					console.log('blur');
-					scope.$apply(model.assign(scope, false));
 				});
 			}
 		};
