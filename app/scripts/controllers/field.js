d668b68f75d4b9ad126e1552e32c0fb8bf70d730
meriadec <meriadec.pillet@gmail.com>
2014-05-17 17:54:12 +0200

    style

--- app/scripts/controllers/field.js
+++ app/scripts/controllers/field.js
@@ -1,11 +1,14 @@
 'use strict';
 
 angular.module('fieldApp')
-	.controller('FieldCtrl', function ($scope, data, $http, $location) {
+	.controller('FieldCtrl', function ($scope, data, $http, $location, $timeout) {
 
 		var original = angular.copy(data);
 
 		$scope.field = data;
+		$scope.addingLogin = false;
+
+		console.log($scope.field);
 
 		$scope.updateField = function () {
 			$http.put('/api/fields/' + $scope.field._id, $scope.field).then(function () {
@@ -25,6 +28,18 @@ angular.module('fieldApp')
 
 		$scope.equal = function () {
 			return angular.equals($scope.field, original);
+		};
+
+		$scope.toggleAddLogin = function () {
+			$scope.addingLogin = !$scope.addingLogin;
+		};
+
+		$scope.addLogin = function (login) {
+			if (login) {
+				$scope.field.corrections.push({ targetName: login });
+			}
+			$scope.newLogin = '';
+			$scope.addingLogin = false;
 		};
 
 	});
